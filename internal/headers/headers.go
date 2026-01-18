8ef563933cfef2bfaf60549e34c3f96081d21147
Blue-Onion <rawatadityasingh963@gmail.com>
2026-01-19 02:11:48 +0530

    parseHeaderLine

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -1,12 +1,62 @@
 package headers
 
-
+import (
+	"bytes"
+	"fmt"
+)
 
 type Headers map[string]string
-func NewHeaders() Headers{
-	return Headers{}
-}
-func (h Headers) Parse(data []byte) (n int, done bool, err error){
-	return 0,false,nil
-}
 
+var sep = []byte("\r\n")
+
+func NewHeaders() Headers {
+
+	return map[string]string{}
+}
+func parseHeader(fldLine []byte) (string, string, error) {
+	fmt.Println("Gone here")
+	parts := bytes.SplitN(fldLine, []byte(":"), 2)
+	if len(parts) != 2 {
+
+		return "", "", fmt.Errorf("Bitch wrong header")
+	}
+	fmt.Println(string(fldLine))
+	key := parts[0]
+	fmt.Println(string(key))
+	if bytes.HasSuffix(key, []byte(" ")) {
+
+		return "", "", fmt.Errorf("Naughty boi wrong field Line")
+	}
+	value := bytes.TrimSpace(parts[1])
+
+	return string(key), string(value), nil
+
+}
+func (h Headers) Parse(data []byte) (int, bool, error) {
+	read := 0
+	done := false
+
+	for {
+		// Host: localhost:42067\r\n\r\n
+		// 0
+		i := bytes.Index(data[read:], sep)
+		if i == -1 {
+			break
+		}
+		//Header Empty
+		if len(data[read : i+read])==0{
+			done = true
+			break
+		}
+		//Read
+		
+		key, value, err := parseHeader(data[read : i+read])
+		if err != nil {
+			return 0, false, err
+		}
+		read += i + len(sep)
+		h[key] = value
+
+	}
+	return read, done, nil
+}
--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -1,5 +1,7 @@
 package headers
+
 import (
+
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,18 +10,19 @@ import (
 func TestHeaderParse(t *testing.T) {
 	// Test: Valid single header
 	headers := NewHeaders()
-	data := []byte("Host: localhost:42067\r\n\r\n")
+	data := []byte("Host: localhost:42067\r\nHost: localhost:42067\r\n\r\n")
 	n, done, err := headers.Parse(data)
 	require.NoError(t, err)
 	require.NotNil(t, headers)
 	assert.Equal(t, "localhost:42067", headers["Host"])
 	assert.Equal(t, 23, n)
-	assert.False(t, done)
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
 	data = []byte("       Host : localhost:42067       \r\n\r\n")
 	n, done, err = headers.Parse(data)
+
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
