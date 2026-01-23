6343968e706a3d116eb95763aa2fea55cf829e9e
Blue-Onion <rawatadityasingh963@gmail.com>
2026-01-23 12:46:34 +0530

    fix

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
+	"crypto/sha256"
+	"fmt"
+	"http/internal/headers"
 	"http/internal/request"
 	"http/internal/response"
 	"http/internal/server"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 )
 
-const port = 42069
+func toStr(s []byte) string {
+	out := ""
+	for _, b := range s {
+		out += fmt.Sprintf("%0.2x", b)
+	}
+	return out
+}
+
+const port = 42067
 
 func request400() []byte {
 
@@ -53,20 +66,56 @@ func request200() []byte {
 func main() {
 	server, err := server.Serve(port, func(w *response.Writer, req *request.Request) *server.HandlerError {
 		h := response.GetDefaultHeaders(0)
-		body:=request200()
-		status:=response.StatusOk
-		switch req.RequestLine.RequestTarget{
-		case "/urBad":
-			status=response.StatusBadRequest
-			body=request500()
-		case  "/urRawat":
-			status=response.StatusBadRequest
-			body=request500()
+		body := request200()
+		status := response.StatusOk
+
+		if req.RequestLine.RequestTarget == "/urBad" {
+			status = response.StatusBadRequest
+			body = request400()
+		} else if req.RequestLine.RequestTarget == "/urRawat" {
+			status = response.StatusInternalServerError
+			body = request500()
+		} else if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin/stream") {
+			target := req.RequestLine.RequestTarget
+			prefix := "/httpbin"
+
+			res, err := http.Get("https://httpbin.org" + target[len(prefix):])
+			if err != nil {
+				status = response.StatusInternalServerError
+				body = request500()
+
+			} else {
+				w.WriteStatusLine(response.StatusOk)
+				h.SET("transfer-encoding", "chunked")
+				h.Replace("content-type", "Text/plain")
+				w.WriteHeaders(*h)
+				fullBody := []byte{}
+				for {
+					data := make([]byte, 32)
+					n, err := res.Body.Read(data)
+					if err != nil {
+						break
+					}
+					fullBody = append(fullBody, data[:n]...)
+					w.WriteBody([]byte(fmt.Sprintf("%x\r\n", n)))
+					w.WriteBody(data[:n])
+					w.WriteBody([]byte("\r\n"))
+				}
+				w.WriteBody([]byte("0\r\n"))
+				tailer:=headers.NewHeaders()
+				out := sha256.Sum256(fullBody)
+				tailer.SET("x-content-sha256",toStr(out[:]))
+				tailer.SET("X-Content-Length", fmt.Sprintf("%d", len(fullBody)))
+				w.WriteHeaders(*tailer)
+
+				w.WriteBody([]byte("\r\n"))
+				return nil
+			}
 		}
-	
+
 		w.WriteStatusLine(status)
-		h.Replace("content-length",strconv.Itoa(len(body)))
-		h.Replace("content-type","text/html")
+		h.Replace("content-length", strconv.Itoa(len(body)))
+		h.Replace("content-type", "Text/html")
 		w.WriteHeaders(*h)
 		w.WriteBody(body)
 		return nil
